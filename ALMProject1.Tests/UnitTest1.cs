c31e26bb4d61883cfe1d381f48782e08a89d73ed
Sandra Dahlberg <42976860+sandrakdahlberg@users.noreply.github.com>
2019-11-13 15:29:09 +0100

    Revert "Added transactions functionality"

--- ALMProject1.Tests/UnitTest1.cs
+++ ALMProject1.Tests/UnitTest1.cs
@@ -1,6 +1,3 @@
-using ALMProject1.Web.Models;
-using System.Linq;
-using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ALMProject1.Tests
@@ -9,43 +6,8 @@ namespace ALMProject1.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestDeposit()
+        public void TestMethod1()
         {
-           var bank = new BankRepository();
-           
-
-           var account = bank.Accounts.SingleOrDefault(x => x.AccountId == 1);
-           var balanceBelore = account.Balance;
-
-           bank.Deposit(100, 1);
-
-           var result = account.Balance;
-
-           Assert.AreEqual((balanceBelore + 100), result);
-        }
-
-        [TestMethod]
-        public void TestWithdraw()
-        {
-            var bank = new BankRepository();
-
-
-            var account = bank.Accounts.SingleOrDefault(x => x.AccountId == 1);
-            var balanceBelore = account.Balance;
-
-            bank.Withdraw(100, 1);
-
-            var result = account.Balance;
-
-            Assert.AreEqual((balanceBelore - 100), result);
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException))]
-        public void TestAmountTooHigh()
-        {
-            var bank = new BankRepository();
-            bank.Withdraw(100000, 1);
         }
     }
 }
